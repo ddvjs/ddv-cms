30875704239a4f2c0a1ad2af648f906c9c053fa1
hadiChan <861883474@qq.com>
2017-04-10 14:41:19 +0800

    edit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import adminHeader from '../packages/adminHeader'
+import adminBreadcrumb from '../packages/adminBreadcrumb'
+import adminSidebar from '../packages/adminSidebar'
 /* istanbul ignore next */
 adminHeader.install = function (Vue) {
   Vue.component(adminHeader.name, adminHeader)
 }
 
+adminBreadcrumb.install = function (Vue) {
+  Vue.component(adminBreadcrumb.name, adminBreadcrumb)
+}
+
+adminSidebar.install = function (Vue) {
+  Vue.component(adminSidebar.name, adminSidebar)
+}
+
 export {
-  adminHeader
+  adminHeader,
+  adminBreadcrumb,
+  adminSidebar
 }
 export default {
-  adminHeader
+  adminHeader,
+  adminBreadcrumb,
+  adminSidebar
 }
