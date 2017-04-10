8fbfa4e869b72281d716999b718a87ca4e8b1afd
hadiChan <861883474@qq.com>
2017-04-10 12:24:15 +0800

    edit sidebar

--- packages/AdminSidebar.vue
+++ packages/AdminSidebar.vue
@@ -1,19 +1,26 @@
-<style scoped>
-    .el-menu--dark{
-        background-color:#263238;
-    }
+<style lang="scss">
+  .ddv-management-system{
+    .sidebars{
+      .tac{
+        .el-menu--dark{
+            background-color:#263238;
+        }
 
-    .el-menu--horizontal.el-menu--dark .el-submenu .el-menu-item.is-active, .el-menu-item.is-active{
-        background-color: #26a69a;
-        color: #fff;
+        .el-menu--horizontal.el-menu--dark .el-submenu .el-menu-item.is-active, .el-menu-item.is-active{
+            background-color: #26a69a;
+            color: #fff;
+        }
+      }
     }
+  }
 </style>
 
 <template>
   <div class="sidebars sidebars-main pull-left" :class="{'active':isFullScreen}">
     <el-row class="tac">
       <el-col :span="24">
-        <el-menu theme="dark" :router="true" :unique-opened="true" :default-active="$route.path">
+        <slot></slot>
+        <!-- <el-menu theme="dark" :router="true" :unique-opened="true" :default-active="$route.path">
           <el-submenu index="1">
             <template slot="title">
               <i class="el-icon-message"></i>
@@ -26,7 +33,7 @@
               关于
             </el-menu-item>
           </el-submenu>
-        </el-menu>
+        </el-menu> -->
       </el-col>
     </el-row>
   </div>
