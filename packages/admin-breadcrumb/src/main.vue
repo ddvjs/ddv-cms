ee4bc502cce3b7525b1bde4c43476c95d7686929
hadiChan <861883474@qq.com>
2017-04-11 23:33:29 +0800

    edit breadcrumb

--- packages/admin-breadcrumb/src/main.vue
+++ packages/admin-breadcrumb/src/main.vue
@@ -14,7 +14,7 @@
       <el-breadcrumb-item :to="{ path: '/admin/' }">
         <i class="fa fa-home mr10"></i>首页
       </el-breadcrumb-item>
-      <el-breadcrumb-item v-for="(data,index) in storeData.breadcrumb" :to="{path:data.path}" :key="index">
+      <el-breadcrumb-item v-for="(data,index) in breadcrumb" :to="{path:data.path}" :key="index">
         <span v-text="data.name"></span>
       </el-breadcrumb-item>
     </el-breadcrumb>
@@ -22,32 +22,20 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
 
 export default {
   name: 'adminBreadcrumb',
-  computed: mapState({
-    storeData: state => state.admin,
-    breadcrumbTitle () {
-      return this.storeData.breadcrumb.slice(this.storeData.breadcrumb.length - 2, this.storeData.breadcrumb.length)
+  props: {
+    'ground': {
+      type: String
     },
-    ground () {
-      if (this.storeData.breadcrumb.length === 1) {
-        let obj = this.storeData.breadcrumb[0]
-        return obj.name
-      } else if (this.storeData.breadcrumb.length >= 2) {
-        let obj = this.storeData.breadcrumb[this.storeData.breadcrumb.length - 2]
-        return obj.name
-      }
-      return ''
+    'nowPage': {
+      type: String
     },
-    nowPage () {
-      if (this.storeData.breadcrumb.length >= 2) {
-        let obj = this.storeData.breadcrumb[this.storeData.breadcrumb.length - 1]
-        return ` - ${obj.name}`
-      }
-      return ''
+    'breadcrumb': {
+      type: Array,
+      default: []
     }
-  })
+  }
 }
 </script>
