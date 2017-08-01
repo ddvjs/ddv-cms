292b1f2b1baf4ce87f87467cbc318dd90c2568d4
hua <yuchonghua@163.com>
2017-08-01 16:32:36 +0800

    fixbug

--- server/index.js
+++ server/index.js
@@ -1,13 +1,12 @@
 'use strict'
 
-const chokidar = require('chokidar')
 const app = require('express')()
 const worker = require('ddv-worker')
 const http = require('http')
-const Nuxt = require('nuxt')
-var config = null
+const {Nuxt, Builder} = require('nuxt')
+const logger = require('../lib/logger')
+var options = null
 var nuxt = null
-var siteConfigFile = null
 
 // 创建http服务
 worker.server = http.createServer(app)
@@ -15,61 +14,56 @@ worker.server = http.createServer(app)
 // 创建编译器的方法
 function createNuxtRender () {
   return (nuxt && nuxt.close ? nuxt.close() : Promise.resolve())
-  .then(() => {
-    // 删除旧的配置
-    if (require.cache && siteConfigFile && require.cache[siteConfigFile]) {
-      delete require.cache[siteConfigFile]
-    }
-    // 引入配置信息-Import and Set Nuxt.js options
-    config = require(siteConfigFile)
-    // 调试模式
-    worker.DEBUG = config.dev = !(process.env.NODE_ENV === 'production')
-  }).then(() => {
+    .then(() => {
     // Init Nuxt.js
-    nuxt = new Nuxt(config)
-    // Build only in dev mode
-    if (config.dev) {
-      // 编译 - Build only in dev mode
-      return nuxt.build()
-    }
-  }).then(() => {
-    console.log('create nuxt render success')
-  }).catch((error) => {
-    console.error(error) // eslint-disable-line no-console
-    process.exit(1)
-  })
+      nuxt = new Nuxt(options)
+      // Build only in dev mode
+      if (options.dev) {
+        const builder = new Builder(nuxt)
+        // 编译 - Build only in dev mode
+        return builder.build()
+      }
+    }).then(() => {
+      logger.log('create nuxt render success')
+    }).catch((error) => {
+      logger.error(error) // eslint-disable-line no-console
+      process.exit(1)
+    })
 }
 module.exports = worker
-worker.serverStart = function serverStart (siteConfigFileInput, configInput) {
-  siteConfigFile = siteConfigFileInput
-  config = configInput
-  chokidar.watch(siteConfigFile, { ignoreInitial: true })
-  .on('all', function () {
-    console.log('You have modified the configuration information and are recompiling')
+worker.serverStart = function serverStart (config) {
+  config.onChange(o => {
+    options = o
+    logger.log('You have modified the configuration information and are recompiling')
     // 重新创建编译器
     createNuxtRender()
   })
-  // 创建编译器
-  createNuxtRender().then(() => {
-    if (config.dev) {
-      // 使用nuxt插件
-      app.use(function (res, req, next) {
-        return nuxt.render.apply(this, arguments)
-      })
-    } else {
-      // 使用nuxt插件
-      app.use(nuxt.render)
-    }
-    // 监听服务 - Listen the server
-    worker.updateServerConf({
-      defaultListen: config.defaultListen,
-      listen: config.listen,
-      cpuLen: config.cpuLen
-    }).then(() => {
-      console.log('监听配置参数 更新成功')
-    }, e => {
-      console.error('监听配置参数 更新失败')
-      console.error(e)
+  config.getConfig()
+    .then(o => {
+      options = o
+      // 创建编译器
+      return createNuxtRender()
     })
-  })
+    .then(() => {
+      if (options.dev) {
+      // 使用nuxt插件
+        app.use(function (res, req, next) {
+          return nuxt.render.apply(this, arguments)
+        })
+      } else {
+      // 使用nuxt插件
+        app.use(nuxt.render)
+      }
+      // 监听服务 - Listen the server
+      worker.updateServerConf({
+        defaultListen: options.defaultListen,
+        listen: options.listen,
+        cpuLen: options.cpuLen
+      }).then(() => {
+        logger.log('监听配置参数 更新成功')
+      }, e => {
+        logger.error('监听配置参数 更新失败')
+        logger.error(e)
+      })
+    })
 }
